8828291af08c04001175ae4a20ee7b34754802e4
Aaron Visser <aaron.visser@gmail.com>
2024-03-15 14:23:10 -0400

    Update setup_newnode.sh

--- setup_newnode.sh
+++ setup_newnode.sh
@@ -56,6 +56,18 @@ sudo systemctl restart docker
 # install iscsi
 sudo apt-get install open-iscsi
 
+# new disk:
+sudo lsblk -o NAME,FSTYPE,SIZE,MOUNTPOINT,LABEl
+sudo parted -s --align optimal /dev/sdb -- mklabel gpt  mkpart primary ext4 0% 100%
+sudo mkfs.ext4 /dev/sdb1
+sudo mkdir -p /mnt/sdb/
+# get UUID
+ sudo blkid
+
+sudo vim /etc/fstab
+# add: UUID=81b527a6-94c7-4663-8c58-d9ff1f74bd47 /mnt/sdb ext4 defaults 0 0
+sudo mount -a
+
 # reboot
 
 
@@ -67,11 +79,7 @@ kubeadm token create --print-join-command
 # a command with parameters will be printed, copy that and run it on the new node
 # it will look something like
 # kubeadm join 130.15.15.128:6443 --token 80e3qa.1xwl44yql5l08t7c --discovery-token-ca-cert-hash sha256:6d61b815f83bfb8970602729c3ba9ebabc403d8167eb405bb0574f84ed269641 
-
-
-
 # once you run that command, hopefully it will join.
-
 # now tags have to be added to the new node. Do this on the master node lobot
 # you can get the list of nodes with
 kubectl get node --all-namespaces
@@ -86,12 +94,20 @@ kubectl get pod -o wide --all-namespaces | grep NODENAMEHERE
 kubectl logs -n PODNAME PODINSTANCE
 
 # add lab tag
-kubectl label nodes NODENAMEHERE lab=caslab 
+kubectl label nodes NODENAMEHERE lab=devlab
 
 # add gpu tag, there is a script here that you can look at how it's automatically done.
 # https://github.com/Queens-School-of-Computing/Lobot/blob/master/setup_master.sh#L88
-kubectl label nodes NODENAMEHERE hardware-type=NVIDIAGPU
+kubectl label nodes $nodename hardware-type=NVIDIAGPU
+# find the PODNAME name of the nvidia daemon, look for the pod name
+kubectl get pod -A -o wide | grep nvidia | grep $nodename
 
-kubectl label nodes NODENAMEHERE nvidia.com/brand=NVIDIA-A40 
+# get the product name of the GPUs
+gpu=$(kubectl exec -it -n kube-system NVIDIAPODNAME — nvidia-smi -q | grep ‘Product Name’ | head -n 1)
+#label=$(echo $gpu | cut -d ‘:’ -f 2 | tr -d ‘\r’ | tr ” ” “-” )
+# seems to add a – at the start?
+label=NVIDIA-RTX-A5000
+#kubectl label nodes ${nodename} nvidia.com/brand=NVIDIA-A40
+kubectl label nodes ${nodename} “nvidia.com/brand=${label}“
 
 # Now reboot your new node and check to see if it's joined successfully! Enjoy!
