d9239a29a4ef11c5e1c843c25deebb33f089519a
Aaron Visser <aaron.visser@gmail.com>
2026-03-27 11:38:00 -0400

    chore: remove because now in separate project.

--- tools/lobot-collector.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Launcher for lobot-collector service.
-# WorkingDirectory must be the tools directory so lobot_tui is importable.
-TOOLS_DIR="${LOBOT_TOOLS_DIR:-/opt/Lobot/tools}"
-exec "$TOOLS_DIR/lobot_collector/.venv/bin/python3" -m lobot_collector
